55c0e7fcc06e244c12cffae9483b9936b5f9eaad
MostAardvark224 <amaybabel@gmail.com>
2025-11-20 23:55:36 -0600

    tailwind setup, remove welcome page

---
+++ app/pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="text-center pt-10 text-[40px]">Welcome To Research Marker!</div>
+  <NuxtLink
+    to="/test"
+    class="block mx-auto text-center pt-2 text-[30px] text-blue-500 underline"
+  >
+    Test
+  </NuxtLink>
+</template>
+<script setup></script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,26 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ['@nuxt/hints', '@nuxt/image', '@nuxt/ui']
-})
+  modules: ["@nuxt/hints", "@nuxt/image", "@nuxt/ui"],
+  ssr: true,
+
+  css: ["~/assets/main.css"],
+
+  app: {
+    head: {
+      title:
+        "Research Marker | Research Paper Annotator | Understand Scientific Papers!",
+
+      htmlAttrs: {
+        lang: "en",
+      },
+    },
+  },
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
