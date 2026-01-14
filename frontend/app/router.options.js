b1e78d1b845f48fbf424d426bcac847cb07222f8
MostAardvark224 <amaybabel@gmail.com>
2026-01-14 02:05:41 -0600

    retrying new build

--- frontend/app/router.options.js
+++ frontend/app/router.options.js
@@ -2,6 +2,8 @@ import { createWebHashHistory, createWebHistory } from "vue-router";
 
 export default {
   history: (base) => {
+    console.log("CreateHistory called. ENV:", process.env.NODE_ENV);
+
     return process.env.NODE_ENV === "development"
       ? createWebHistory(base)
       : createWebHashHistory();
